970f1f7dfee27e8ad557c81e1d2d77a98961f74f
Kiselev <mr.nik.kiselev.020202@gmail.com>
2023-12-05 14:46:16 +0300

    fillTableCells add dataElementId

--- applications/static/applications/js/fillTableCells.js
+++ applications/static/applications/js/fillTableCells.js
@@ -13,12 +13,14 @@ function fillTableCells(data) {
     data.forEach(function(item) {
         var cellClass = item.cellName;
         var parentCell = item.group;
+        var dataElementId = item.dataElementId;
         var cells = document.getElementsByClassName(cellClass);
         if (cells.length > 0) {
             for (var i = 0; i < cells.length; i++) {
                 var parentText = cells[i].parentElement.textContent.trim();
                 var parts = parentText.split(/\s+/);
                 if (cells[i].classList.value === cellClass && parentCell === parts[0]) {
+                    cells[i].dataset.id = dataElementId;
                     cells[i].textContent = item.text;
                 }
             }
